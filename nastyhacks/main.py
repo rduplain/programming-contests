202f6e3ca3ef14ca6abde35072bcf1ee30faab8d
Ron DuPlain <ron.duplain@gmail.com>
2017-10-27 20:16:01 -0400

    Solve nastyhacks.

---
+++ nastyhacks/main.py
@@ -0,0 +1,23 @@
+import sys
+
+
+def advertise(fd, i):
+    r, e, c = fd.readline().strip().split(' ')
+    r, e, c = int(r), int(e), int(c)
+
+    with_ad = e - c
+    if with_ad > r:
+        return 'advertise'
+    elif with_ad == r:
+        return 'does not matter'
+    else:
+        return 'do not advertise'
+
+
+def main(fd):
+    count = int(fd.readline())
+    for i in range(1, count+1):
+        print(advertise(fd, i))
+
+
+main(sys.stdin)
