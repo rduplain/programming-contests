f52efb58dccbcd7219bd6d276a351ab61110838c
Ron DuPlain <ron.duplain@gmail.com>
2017-10-28 11:05:05 -0400

    Use fewer variables.

--- closingtheloop/main.py
+++ closingtheloop/main.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def reverse_sorted(*a):
+def rsorted(*a):
     "Improve readability when using generators."
     return sorted(*a, reverse=True)
 
@@ -9,10 +9,11 @@ def reverse_sorted(*a):
 def rope(fd):
     s = int(fd.readline().strip())
     xs = fd.readline().strip().split(' ')
-    r = reverse_sorted(int(x.rstrip('R')) for x in xs if x.endswith('R'))
-    b = reverse_sorted(int(x.rstrip('B')) for x in xs if x.endswith('B'))
-    # Subtract 1cm for each segment.
-    return sum(ri + bi - 2 for ri, bi in zip(r, b))
+    return sum(
+        # Subtract 1cm for each segment.
+        ri + bi - 2 for ri, bi
+        in zip(rsorted(int(x.rstrip('R')) for x in xs if x.endswith('R')),
+               rsorted(int(x.rstrip('B')) for x in xs if x.endswith('B'))))
 
 
 def main(fd):
