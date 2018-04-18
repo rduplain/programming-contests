0cd829ffbb7971150dc9d97ecee017cd22a5920f
Ron DuPlain <ron.duplain@gmail.com>
2018-04-17 20:12:06 -0400

    Improve solution.

--- tolower/main.py
+++ tolower/main.py
@@ -5,25 +5,18 @@ def read_n_lines(fd, n):
     return [fd.readline().strip() for i in range(n)]
 
 
-def foo(fd, p, t):
+def tolower(fd, p, t):
     for line in read_n_lines(fd, t):
         if not line[1:].islower():
             return False
     return True
 
 
-def tolower(fd):
+def main(fd):
     p, t = fd.readline().strip().split(' ')
     p, t = int(p), int(t)
-    count = 0
-    for x in range(p):
-        if foo(fd, p, t):
-            count += 1
-    print(count)
 
-
-def main(fd):
-    tolower(fd)
+    print(sum(map(int, [tolower(fd, p, t) for x in range(p)])))
 
 
 if __name__ == '__main__':
